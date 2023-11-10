ab57e5d8fbad2118633159384d67dbd9b9d607b8
Nikita <np.shche@gmail.com>
2023-11-11 01:39:41 +0300

    feat: complete task 2

--- Assets/Scripts/Tasks/Task2/ObjectsInteractionTask2.cs
+++ Assets/Scripts/Tasks/Task2/ObjectsInteractionTask2.cs
@@ -1,6 +1,18 @@
+using System;
 using UnityEngine;
 
 public class ObjectsInteractionTask2 : MonoBehaviour
 {
     // TODO: В методе Awake создайте на сцене в точке LampRoot одну из наборов ламп (из папки Prefabs/Lamps)    
+
+    [SerializeField]
+    private GameObject _prefab;
+
+    [SerializeField]
+    private Transform _parent;
+
+    private void Awake()
+    {
+        Instantiate(_prefab, _parent);
+    }
 }
